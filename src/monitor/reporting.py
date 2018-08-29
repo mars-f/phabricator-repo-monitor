7459274c70cb46fc81230dcb1cb47be2580dca22
Mars <mfogels@gmail.com>
2018-08-28 20:41:05 -0400

    Add debug logging to reporting code

--- src/monitor/reporting.py
+++ src/monitor/reporting.py
@@ -2,11 +2,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """Functions for reporting this program's results"""
+import logging
+
 import click
 from datadog import statsd
 
 from monitor.config import Mirror
 from monitor.main import ReplicationStatus
+
+log = logging.getLogger(__name__)
 
 
 def print_replication_lag(_, replication_status: ReplicationStatus):
@@ -23,6 +27,7 @@ def print_replication_lag(_, replication_status: ReplicationStatus):
 
 def report_to_statsd(mirror: Mirror, replication_status: ReplicationStatus):
     repo_label = mirror.repo_callsign.lower()
+    log.debug("sending to datadog: " + f"phabricator.repository.{repo_label}.seconds_behind_source_repo " + str(replication_status.seconds_behind))
     statsd.gauge(
         f"phabricator.repository.{repo_label}.seconds_behind_source_repo",
         replication_status.seconds_behind,
